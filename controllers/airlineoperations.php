e877e68341ce12f3ca6bd881516084f857060506
Richard Onyango <akellorich@gmail.com>
2025-08-18 10:48:54 +0300

    Added airtline MVC components

---
+++ controllers/airlineoperations.php
@@ -0,0 +1,7 @@
+<?php
+    require_once("../models/airlines.php");
+
+    $airline=new airline();
+
+    
+?>
---
+++ models/airlines.php
@@ -0,0 +1,25 @@
+<?php
+    require_once("db.php");
+
+    class airline extends db{
+        function checkairline(){
+
+        }
+
+        function saveairline(){
+
+        }
+
+        function getairlines(){
+
+        }
+
+        function getairlinedetails(){
+
+        }
+
+        function deleteairline(){
+            
+        }
+    }
+?>
